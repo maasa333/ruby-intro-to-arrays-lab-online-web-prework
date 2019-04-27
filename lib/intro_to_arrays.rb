e21e36e8aeaf5cbf8aec9015992b9ec76eb80407
Maasa Kono <konomaasa@gmail.com>
2019-04-27 22:35:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,6 +10,6 @@ end
 
 def arrays_indexing
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = taylor_swift
+  taylor_swift = my_first_element
   my_first_element.take(1)
 end
